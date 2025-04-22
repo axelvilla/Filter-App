d1b9e52173b00fa633aabcaeddb323d19e8b4ced
julian-casalis <escjuliancasalis@gmail.com>
2025-04-22 16:39:44 -0300

    julian hizo macana

--- lupita.js
+++ lupita.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
         const tbody = document.querySelector('#tabla tbody');
         const buscador = document.querySelector('#buscador');
         
-        // Función para realizar la búsqueda
+    
         function buscarTabla() {
           const textoBusqueda = buscador.value.toLowerCase();
-          tbody.innerHTML = '';  // Limpiar el tbody antes de actualizar
+          tbody.innerHTML = '';  
           
           data.forEach((fila, i) => {
             const esCoincidente = fila.provincia.toLowerCase().includes(textoBusqueda) ||
@@ -30,10 +30,10 @@ document.addEventListener('DOMContentLoaded', function () {
           });
         }
   
-        // Inicializar la tabla al cargar
+       
         buscarTabla();
         
-        // Agregar evento de cambio de búsqueda
+  
         buscador.addEventListener('input', buscarTabla);
       })
       .catch(error => console.error('Error fetching data:', error));
