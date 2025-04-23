6a649445b7f26dc5c2581f8136cbfaf945e4f8f1
axelvilla <axelvilla746@gmail.com>
2025-04-23 16:50:56 -0300

    "Se modularizo la parte de los botones de las columnas usando javascript"

---
+++ icons.js
@@ -0,0 +1,11 @@
+const sortButton = `
+<svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#1f1f1f">
+  <path d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z"/>
+</svg>
+`;
+
+const buttons = document.querySelectorAll(".button_sort");
+
+buttons.forEach(buttons => {
+    buttons.innerHTML = sortButton;
+})
