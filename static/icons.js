aa575b7e46e22f40859e5e3d473add0af6cb5fa5
axelvilla <axelvilla746@gmail.com>
2025-04-24 15:48:31 -0300

    Se arreglo los botones de ordenamiento, la tabla y el boton para mostrar los resultados en forma grafica

--- static/icons.js
+++ static/icons.js
@@ -1,5 +1,5 @@
 const sortButton = `
-<svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 -960 960 960" width="10px" fill="#1f1f1f">
+<svg xmlns="http://www.w3.org/2000/svg" height="15px" viewBox="0 -960 960 960" width="15px" fill="#1f1f1f">
   <path d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z"/>
 </svg>
 `;
