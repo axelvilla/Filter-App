db0dbdf58ace26373d3c823c57e888854c6711e6
Axel Villa <118828431+axelvilla@users.noreply.github.com>
2025-04-24 04:38:10 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 def obtener_conexion():
     return mysql.connector.connect(
         host='localhost',
-        user='root',
-        password='1234',
+        user='', #Poner su usuario
+        password='', #Poner su contraseña
         database='provincias'
     )
